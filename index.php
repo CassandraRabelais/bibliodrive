38c9e9f887d00fedb312a8d4fb346acd2bf10ab1
Cassy Leclerc <leclerc.cassandra09@gmail.com>
2025-12-05 15:02:49 +0100

    Projet v.3

--- index.php
+++ index.php
@@ -8,17 +8,19 @@
 <html>
 </head>
 <body>
-	La bibliothèque de Moulinsart est fermée au public jusqu'à nouvel ordre. Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive !
+	<div class="alert alert-info text-center my-3">
+		La bibliothèque de Moulinsart est fermée au public jusqu'à nouvel ordre. Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive !
+	</div>
 
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
 			<nav class="navbar navbar-expand-sm bg-dark navbar-dark py-2">
-				<form class="form-inline d-flex gap-2 w-75 mx-auto" action="/action_page.php">
-					<input class="form-control flex-grow-1" type="text" placeholder="Rechercher dans le catalogue (saisir le nom de l'auteur)" style="background-color: #e7f3ff; height: 32px;">
+				<form class="form-inline d-flex gap-2 w-75 mx-auto" action="index.php" method="get">
+					<input name="author" class="form-control flex-grow-1" type="text" placeholder="Rechercher dans le catalogue (saisir le nom de l'auteur)" style="background-color: #e7f3ff; height: 32px;" value="<?php echo isset($_GET['author']) ? htmlspecialchars(trim($_GET['author'])) : ''; ?>">
 					<button class="btn btn-success" type="submit">Recherche</button>
 				</form>
-			</nav><br>			
+			</nav><br>            
 		</div>
 	</div>
 	<div class="row">
