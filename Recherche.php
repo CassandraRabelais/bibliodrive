48966c446ee1cdf18dda5552ae309fc5b52b31a7
Cassy Leclerc <leclerc.cassandra09@gmail.com>
2025-12-12 14:29:11 +0100

    Projet v..4

--- Recherche.php
+++ Recherche.php
@@ -4,6 +4,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="style.css" rel="stylesheet">
 	<title>Résultats de recherche</title>
 </head>
 <body>
